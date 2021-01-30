2f8de5a967984c3ac81b255bf6ae5a9295b691d9
Anonimvendeta <kirillmanulik@gmail.com>
2021-01-30 19:10:43 +0300

    Kirill

--- src/main/java/by/adukar/kirill/HellotoAll.java
+++
@@ -1,7 +0,0 @@
-package by.adukar.kirill;
-
-public class HellotoAll {public static void main(String[] args) {
-    System.out.println("Hello to all of you ");
-}
-}
-
--- src/main/java/by/adukar/kirill/KirillSayHello.java
+++
@@ -1,7 +0,0 @@
-package by.adukar.kirill;
-
-public class KirillSayHello {
-    public static void main(String[] args) {
-        System.out.println("Zig Hail my Fuhrer");
-    }
-}
