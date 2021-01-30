1773b5d86b1f7e050f7c8304e5955e08c057ba44
Antony <fvantop@gmail.com>
2021-01-30 17:17:34 +0300

    Kupreichik A. add first task

--- src/main/java/by/adukar/Application.java
+++ src/main/java/by/adukar/Application.java
@@ -3,5 +3,21 @@ package by.adukar;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        int num = 2;
+        int s = 2;
+
+        int[] myNumbers = new int[10];
+        for(int i=0;i<=9;i++){
+            myNumbers[i] = s * num ;
+            s = s * 2;
+        }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.print(myNumbers[i] + " ");
+        }
     }
+
+
+
+
 }
